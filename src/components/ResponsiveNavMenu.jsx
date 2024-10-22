e889aeab19f6c8b7e2405892c5bd95c884714a9f
Vannn03 <90308438+Vannn03@users.noreply.github.com>
2024-10-22 12:10:35 +0700

    fix: folder structure

--- src/components/ResponsiveNavMenu.jsx
+++ src/components/ResponsiveNavMenu.jsx
@@ -2,29 +2,27 @@ import { LiaTimesSolid } from "react-icons/lia";
 import { TbMenuDeep } from "react-icons/tb";
 import PropTypes from "prop-types";
 
-const ResponsiveNavMenu = ({ toggleNav, setToggleNav }) => {
-  return (
-    <>
-      <TbMenuDeep
-        className={`absolute right-8 flex size-8 cursor-pointer transition-all duration-200 xs:size-9 ss:hidden ${
-          toggleNav ? "-rotate-90 opacity-0" : "rotate-0 opacity-100"
-        }`}
-        onClick={() => {
-          setToggleNav((prev) => !prev);
-        }}
-      />
+const ResponsiveNavMenu = ({ toggleNav, setToggleNav }) => (
+  <>
+    <TbMenuDeep
+      className={`absolute right-8 flex size-8 cursor-pointer transition-all duration-200 xs:size-9 ss:hidden ${
+        toggleNav ? "-rotate-90 opacity-0" : "rotate-0 opacity-100"
+      }`}
+      onClick={() => {
+        setToggleNav((prev) => !prev);
+      }}
+    />
 
-      <LiaTimesSolid
-        className={`absolute right-8 size-8 cursor-pointer transition-all duration-200 xs:size-9 ss:hidden ${
-          toggleNav ? "-rotate-90 opacity-100" : "rotate-0 opacity-0"
-        }`}
-        onClick={() => {
-          setToggleNav((prev) => !prev);
-        }}
-      />
-    </>
-  );
-};
+    <LiaTimesSolid
+      className={`absolute right-8 size-8 cursor-pointer transition-all duration-200 xs:size-9 ss:hidden ${
+        toggleNav ? "-rotate-90 opacity-100" : "rotate-0 opacity-0"
+      }`}
+      onClick={() => {
+        setToggleNav((prev) => !prev);
+      }}
+    />
+  </>
+);
 
 export default ResponsiveNavMenu;
 
