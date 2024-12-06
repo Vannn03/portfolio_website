c9abff4ce4dbe56fd254ecea1e338b985ab441ea
Vannn03 <90308438+Vannn03@users.noreply.github.com>
2024-12-06 10:41:14 +0700

    fix: bug in theme toggler

--- src/components/ThemeToggler.jsx
+++ src/components/ThemeToggler.jsx
@@ -3,15 +3,17 @@ import PropTypes from "prop-types";
 
 const ThemeToggler = ({ setTheme, theme }) => (
   <div
-    className={`fixed bottom-10 right-8 flex cursor-pointer flex-col gap-1 rounded-full bg-navbar p-1 text-3xl text-accent shadow-theme-toggler-glow ss:right-16 ss:flex-row ss:gap-2 ss:p-2 ss:text-4xl md:right-1/2 md:translate-x-1/2`}
+    className={`fixed bottom-10 right-8 z-50 cursor-pointer rounded-full bg-navbar text-3xl text-accent shadow-theme-toggler-glow ss:right-16 ss:gap-2 ss:text-4xl md:right-1/2 md:translate-x-1/2`}
     onClick={() => setTheme((prev) => !prev)}
   >
-    <FaMoon
-      className={`rounded-full p-2 transition-all duration-200 ${theme ? "translate-y-6 rotate-180 opacity-0 ss:translate-x-6 ss:translate-y-0" : "rotate-0 bg-primary opacity-100"}`}
-    />
-    <FaSun
-      className={`rounded-full p-2 transition-all duration-200 ${theme ? "rotate-0 bg-primary opacity-100" : "-translate-y-6 -rotate-180 opacity-0 ss:-translate-x-6 ss:-translate-y-0"}`}
-    />
+    <span className="flex flex-col gap-1 p-1 ss:flex-row ss:p-2">
+      <FaMoon
+        className={`rounded-full p-2 transition-all duration-200 ${theme ? "translate-y-6 rotate-180 opacity-0 ss:translate-x-6 ss:translate-y-0" : "rotate-0 bg-primary opacity-100"}`}
+      />
+      <FaSun
+        className={`rounded-full p-2 transition-all duration-200 ${theme ? "rotate-0 bg-primary opacity-100" : "-translate-y-6 -rotate-180 opacity-0 ss:-translate-x-6 ss:-translate-y-0"}`}
+      />
+    </span>
   </div>
 );
 
